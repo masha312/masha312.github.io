5fde0b97a2d8ffa4178a0c9a0c00feb903add6ab
Maria Shaposhnikova <maria.shaposhnikova@logicmanager.com>
2020-04-18 16:35:23 -0400

    updates to metadata

--- lyriks/md.js
+++ lyriks/md.js
@@ -1,39 +1,9 @@
-const line1 = "Break the silence Face the violence Let the people speak The moments our's To stop the powers Then we'll all be free";
-const line1text = new Array(50).fill(line1).join(' ');
-const block1 = document.querySelector('.block1');
-block1.innerHTML = line1text;
+const lines = document.querySelectorAll(".line");
 
-const line2 = "Information Information Communication Corporation Commercialization Brainwashes the population";
-const line2text = new Array(50).fill(line2).join(' ');
-const block2 = document.querySelector('.block2');
-block2.innerHTML = line2text;
-
-const line3 = "Tap the wire Start the fire Breaking down this wall Take the system Spare the victims Watch it crash and fall";
-const line3text = new Array(50).fill(line3).join(' ');
-const block3 = document.querySelector('.block3');
-block3.innerHTML = line3text;
-
-const line4 = "Information Information Communication Organizations Across the nations Reaching for domination";
-const line4text = new Array(50).fill(line4).join(' ');
-const block4 = document.querySelector('.block4');
-block4.innerHTML = line4text;
-
-const line5 = "I know they see it I know they hear it I know they're watching it I know they're near it I know they fear it There is no stopping it";
-const line5text = new Array(50).fill(line5).join(' ');
-const block5 = document.querySelector('.block5');
-block5.innerHTML = line5text;
-
-const line6 = "Information Information Communication Information Information Communication It feels like we're losing control";
-const line6text = new Array(50).fill(line6).join(' ');
-const block6 = document.querySelector('.block6');
-block6.innerHTML = line6text;
-
-const line7 = "Computers polluting your soul Just shut up and do what you're told And see what the future will hold";
-const line7text = new Array(50).fill(line7).join(' ');
-const block7 = document.querySelector('.block7');
-block7.innerHTML = line7text;
-
-const line8 = "It feels like we're losing control Computers polluting your soul Just shut up and do what you're told And see what the future will hold";
-const line8text = new Array(50).fill(line8).join(' ');
-const block8 = document.querySelector('.block8');
-block8.innerHTML = line8text;
+lines.forEach((line, index) => {
+  const lineText = line.innerText;
+  const lineRepeated = new Array(50).fill(lineText).join(' ');
+  console.log(lineRepeated);
+  const block = document.querySelector(`.block${index + 1}`)
+  block.innerHTML = lineRepeated;
+});
