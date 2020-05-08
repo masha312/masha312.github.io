be5d54b488648e3c8b8142deb7202c875db75272
Maria Shaposhnikova <maria.shaposhnikova@logicmanager.com>
2020-05-08 16:23:52 -0400

    cleaning up

--- lyriks/md.js
+++
@@ -1,9 +0,0 @@
-const lines = document.querySelectorAll(".line");
-
-lines.forEach((line, index) => {
-  const lineText = line.innerText;
-  const lineRepeated = new Array(50).fill(lineText).join(' ');
-  console.log(lineRepeated);
-  const block = document.querySelector(`.block${index + 1}`)
-  block.innerHTML = lineRepeated;
-});
