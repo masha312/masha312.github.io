eceded6501204fe7632f68bf359775f6c68cc5c6
Maria Shaposhnikova <maria.shaposhnikova@logicmanager.com>
2021-11-20 12:41:54 -0500

    adj

--- js/appear.js
+++ js/appear.js
@@ -4,26 +4,3 @@ lines.forEach((line, index) => {
     const delay = index * 50;
     line.style.animationDelay = `${delay}ms`;
 })
-
-let currentX = 0
-let currentY = 0
-let aimX = 0
-let aimY = 0
-
-const updatePosition = function () {
-    currentX += (aimX - currentX) * 0.1
-    currentY += (aimY - currentY) * 0.1
-
-	  const div = document.querySelector(".screenshot")
-
-    div.style.transform = `rotateX(${-10 * currentY}deg) rotateY(${10 * currentX}deg)`
-
-    requestAnimationFrame(updatePosition)
-}
-
-  updatePosition()
-
-  document.addEventListener("mousemove", function (event) {
-    aimX = (event.pageX - (window.innerWidth / 2)) / window.innerWidth
-    aimY = (event.pageY - (window.innerHeight / 2)) / window.innerHeight
-  })
